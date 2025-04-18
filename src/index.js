fa5f151d6d2d6a48e864a1ee8303f6662c5db8b5
Carthy Shelton <carthys@protonmail.com>
2025-04-18 17:49:47 +0000

    Updates in Replit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { SkylanderProvider } from './context/SkylanderContext';
-
+import { SkylanderProvider } from './context/SkylanderContext.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
